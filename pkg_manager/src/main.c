e2b9e1ffd09541341cdaf80cb89e77398e5d4b24
Loran <yeetnonsens123@gmail.com>
2026-04-08 00:20:40 +0200

    add package manager script

---
+++ pkg_manager/src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    // here should be the code for the package manager but i want to go to sleep right now and also keep the streak on github
+}
